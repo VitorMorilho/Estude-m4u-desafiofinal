a4ee899c656a8ee4f294b8e0a5714866f99c3344
Robson Rosa <contatorobsonrosa@gmail.com>
2021-11-05 17:50:05 -0300

    criado classes DTO

--- src/main/java/com/m4u/estude/dto/AddressDTO.java
+++ src/main/java/com/m4u/estude/dto/AddressDTO.java
@@ -1,4 +1,16 @@
 package com.m4u.estude.dto;
 
+import com.m4u.estude.model.Address;
+import lombok.Getter;
+
+@Getter
 public class AddressDTO {
+    private String street;
+    private String city;
+    private String state;
+    private Integer zipCode;
 }
+
+//public Address addressDto(){
+//    return new Address(street, city, state, zipCode);
+//}
